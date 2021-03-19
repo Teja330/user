3783d75d7b6d7d7672885a737882d6090e4039da
Teja330 <71312129+Teja330@users.noreply.github.com>
2021-03-20 00:48:24 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e)=>{
+  e.preventDefault();
+  console.log(e);
+alert("Hey User How Are You");
+  //document.getElementById('my-form').style.background = '#ccc';
+  //document.querySelector('body').classList.add('bg-dark');
+  //ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
+});
